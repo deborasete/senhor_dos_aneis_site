84a07351095b36d644b4bc62d3e41379972e7fee
Débora Sete Acosta <sete.debora@gmail.com>
2025-01-21 15:15:36 -0300

    creates hero and imports font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest('./dist/css'));
-
 }
 
-exports.default = styles;
+function assets() {
+    return gulp.src('./src/assets/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/assets'));
+}
+
+exports.default = gulp.parallel(styles, assets);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
